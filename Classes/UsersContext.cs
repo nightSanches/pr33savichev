e7331b1311d88275f6fa25470323ebd723867cfe
nightSanches <galesavich@gmail.com>
2024-05-23 19:50:24 +0500

    Разработанные контексты данных. савичев

---
+++ Classes/UsersContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using pr33savichev.Classes.Common;
+using pr33savichev.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pr33savichev.Classes
+{
+    public class UsersContext : DbContext
+    {
+        public DbSet<Users> Users { get; set; }
+        public UsersContext() => Database.EnsureCreated();
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => optionsBuilder.UseSqlServer(Config.config);
+    }
+}
