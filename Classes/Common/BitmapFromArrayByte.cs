2ac2528b7e7e23673cc17df5e350d88b09ed4add
nightSanches <galesavich@gmail.com>
2024-05-23 19:48:38 +0500

    Разработанные общие классы. Савичев

---
+++ Classes/Common/BitmapFromArrayByte.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace pr33savichev.Classes.Common
+{
+    public class BitmapFromArrayByte
+    {
+        public static BitmapImage LoadImage(byte[] imageData)
+        {
+            BitmapImage Image = new BitmapImage();
+            using (var Stream = new MemoryStream(imageData))
+            {
+                Stream.Position = 0;
+                Image.BeginInit();
+                Image.CreateOptions = BitmapCreateOptions.PreservePixelFormat;
+                Image.CacheOption = BitmapCacheOption.OnLoad;
+                Image.UriSource = null;
+                Image.StreamSource = Stream;
+                Image.EndInit();
+            }
+            Image.Freeze();
+            return Image;
+        }
+    }
+}
---
+++ Classes/Common/Config.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pr33savichev.Classes.Common
+{
+    public class Config
+    {
+        public static readonly string config = "Server=***;Trusted_Connection=False;Database=***;User=***;Pwd=***;";
+    }
+}
