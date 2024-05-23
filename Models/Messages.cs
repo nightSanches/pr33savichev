5f7dfa65781a448021d26174c756fa0939a5fb70
nightSanches <galesavich@gmail.com>
2024-05-23 19:47:07 +0500

    Разработанные модели. Savichev

---
+++ Models/Messages.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pr33savichev.Models
+{
+    public class Messages
+    {
+        public int Id { get; set; }
+        public int UserFrom { get; set; }
+        public int UserTo { get; set; }
+        public string Message { get; set; }
+        public DateTime DateSending { get; set; }
+
+        public Messages(int UserFrom, int UserTo, string Message, DateTime DateSending)
+        {
+            this.UserFrom = UserFrom;
+            this.UserTo = UserTo;
+            this.Message = Message;
+            this.DateSending = DateSending;
+        }
+    }
+}
---
+++ Models/Users.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pr33savichev.Models
+{
+    public class Users
+    {
+        public int Id { get; set; }
+        public string Lastname { get; set; }
+        public string Firstname { get; set; }
+        public string Surname { get; set; }
+        public byte[] Photo { get; set; }
+        public DateTime Online { get; set; }
+
+        public Users(string Lastname, string Firstname, string Surname, byte[] Photo, DateTime Online)
+        {
+            this.Lastname = Lastname;
+            this.Firstname = Firstname;
+            this.Surname = Surname;
+            this.Photo = Photo;
+            this.Online = Online;
+        }
+
+        public string ToFIO()
+        {
+            return $"{Lastname} {Firstname} {Surname}";
+        }
+    }
+}
