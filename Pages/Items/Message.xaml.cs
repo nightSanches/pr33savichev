e76274647e12f3857f37469a6272cec9705026d8
nightSanches <galesavich@gmail.com>
2024-05-23 20:17:24 +0500

    Разработанный пользовательский элемент Message. Савичев

---
+++ Pages/Items/Message.xaml.cs
@@ -0,0 +1,33 @@
+﻿using pr33savichev.Classes.Common;
+using pr33savichev.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace pr33savichev.Pages.Items
+{
+    /// <summary>
+    /// Логика взаимодействия для Message.xaml
+    /// </summary>
+    public partial class Message : UserControl
+    {
+        public Message(Messages messages, Users UserFrom)
+        {
+            InitializeComponent();
+            imgUser.Source = BitmapFromArrayByte.LoadImage(UserFrom.Photo);
+            FIO.Content = UserFrom.ToFIO();
+            tbMessage.Text = messages.Message;
+        }
+    }
+}
