3146af82782d056fafae8e11ca8b20f12e07de85
nightSanches <galesavich@gmail.com>
2024-05-24 19:37:46 +0500

    Выполненная практическая работа на оценку удовлетворительно

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,13 +1,6 @@
-﻿using System.Text;
+﻿using pr33savichev.Models;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace pr33savichev
 {
@@ -16,9 +9,17 @@ namespace pr33savichev
     /// </summary>
     public partial class MainWindow : Window
     {
+        public static MainWindow Instance;
+        public Users LoginUser = null;
+
         public MainWindow()
         {
             InitializeComponent();
+            Instance = this;
+            OpenPages(new Pages.Login());
         }
+
+        public void OpenPages(Page pages) =>
+            frame.Navigate(pages);
     }
 }
